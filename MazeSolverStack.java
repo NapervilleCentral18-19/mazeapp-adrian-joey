cc8351cdfcde9bbdaa83f8d70614b2c91b700b20
adriantse136 <54153247+adriantse136@users.noreply.github.com>
2020-12-08 12:19:06 -0600

    Names

--- MazeSolverStack.java
+++ MazeSolverStack.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class MazeSolverStack here.
  *
- * @author (your name)
+ * @Adrian Tse, Joey Rubas
  * @version (a version number or a date)
  */
 public class MazeSolverStack extends MazeSolver
